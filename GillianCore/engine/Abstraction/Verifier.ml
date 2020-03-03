2febba2fc766baef44626d02db67a97e1fd80f52
Petar Maksimović <PetarMax@users.noreply.github.com>
2020-03-03 16:15:43 +0000

    fix verification, fix coloring, remove unnecessary prints (#10)
    * fix verification, fix coloring, remove unnecessary prints
    
    * fix formatting

--- GillianCore/engine/Abstraction/Verifier.ml
+++ GillianCore/engine/Abstraction/Verifier.ml
@@ -20,7 +20,7 @@ struct
 
   let print_success_or_failure success =
     if success then Fmt.pr "%a" (Fmt.styled `Green Fmt.string) "Success\n"
-    else Fmt.pr "%a" (Fmt.styled `Green Fmt.string) "Failure\n"
+    else Fmt.pr "%a" (Fmt.styled `Red Fmt.string) "Failure\n"
 
   type t = {
     name : string;
