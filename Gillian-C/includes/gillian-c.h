a7dcb9c48e854a7f8c4bf81cc8850dccb2913fec
Sacha Ayoun <sachaayoun@gmail.com>
2020-03-09 17:02:29 +0000

    fix warnings in collections-c

--- Gillian-C/includes/gillian-c.h
+++ Gillian-C/includes/gillian-c.h
@@ -1,2 +1,7 @@
 void ASSUME(int boolean);
 void ASSERT(int boolean);
+
+/* Silence the "unsupported compiler" from apple's implementation */
+#ifndef __GNUC__
+  #define __GNUC__ 4
+#endif
---
+++ Gillian-C/includes/string.h
@@ -0,0 +1,1 @@
+int strcmp (const char * str1, const char * str2);
