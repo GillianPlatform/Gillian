3a98de8730c381e97f4d2a74c506de5620929dd2
Radu Lacraru <40798022+raduml97@users.noreply.github.com>
2020-05-01 13:33:25 +0300

    Logging: phase decorator (#37)
    * Logging: esy add @opam/cinaps
    
    Signed-off-by: Radu Lacraru <radu.lacraru97@gmail.com>
    
    * Logging: add phase decorator
    
    Decorator intended to open a new logging phase when executing a
    function and then close it after the function returns.
    
    Signed-off-by: Radu Lacraru <radu.lacraru97@gmail.com>

--- GillianCore/logging/logging.mli
+++ GillianCore/logging/logging.mli
@@ -43,3 +43,59 @@ val verbose_phase : Report.phase -> unit
 val tmi_phase : Report.phase -> unit
 
 val end_phase : Report.phase -> unit
+
+(*$
+  let () =
+    print_newline ();
+    let dec i =
+      let args =
+        let rec aux = function
+          | j when j == i + 1 -> Printf.sprintf "'a%d" j
+          | j -> Printf.sprintf "'a%d -> %s" j @@ aux @@ (j + 1)
+        in
+        aux 1
+      in
+      Printf.printf "val withPhase%d : Mode.level -> Report.phase -> (%s) -> %s"
+        i args args;
+      print_newline ()
+    in
+    for i = 1 to 5 do
+      dec i
+    done
+*)
+val withPhase1 : Mode.level -> Report.phase -> ('a1 -> 'a2) -> 'a1 -> 'a2
+
+val withPhase2 :
+  Mode.level -> Report.phase -> ('a1 -> 'a2 -> 'a3) -> 'a1 -> 'a2 -> 'a3
+
+val withPhase3 :
+  Mode.level ->
+  Report.phase ->
+  ('a1 -> 'a2 -> 'a3 -> 'a4) ->
+  'a1 ->
+  'a2 ->
+  'a3 ->
+  'a4
+
+val withPhase4 :
+  Mode.level ->
+  Report.phase ->
+  ('a1 -> 'a2 -> 'a3 -> 'a4 -> 'a5) ->
+  'a1 ->
+  'a2 ->
+  'a3 ->
+  'a4 ->
+  'a5
+
+val withPhase5 :
+  Mode.level ->
+  Report.phase ->
+  ('a1 -> 'a2 -> 'a3 -> 'a4 -> 'a5 -> 'a6) ->
+  'a1 ->
+  'a2 ->
+  'a3 ->
+  'a4 ->
+  'a5 ->
+  'a6
+
+(*$*)
