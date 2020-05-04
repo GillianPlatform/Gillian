e0ed0bfdd41fe2d329983a658b4ef489d17ee2db
Radu Lacraru <radu.lacraru97@gmail.com>
2020-05-04 15:17:02 +0000

    Logging: turn reporters into objects
    Allows for context of different reporters to be retained. Will be
    useful when implementing target language specific reports, thus
    reporter behavior will need implementation for such reports.
    
    Signed-off-by: Radu Lacraru <radu.lacraru97@gmail.com>

--- GillianCore/logging/reporter.ml
+++ GillianCore/logging/reporter.ml
@@ -1,57 +1,71 @@
-type t = { log : 'a. 'a Report.t -> unit; wrap_up : unit -> unit }
+type t = < log : 'a. 'a Report.t -> unit ; wrap_up : unit -> unit >
 
-let file_reporter () =
+class file_reporter () =
   let out_channel = open_out "out.log" in
   let formatter = Format.formatter_of_out_channel out_channel in
-  let log : 'a. 'a Report.t -> unit =
-   fun report ->
-    match report.content with
-    | Debug msgf  -> msgf @@ fun fmt -> Format.fprintf formatter @@ fmt ^^ "@,@?"
-    | Phase phase ->
-        Format.fprintf formatter "*** Phase %s ***@,@?"
-        @@ Report.string_of_phase phase
-  in
-  let wrap_up () = close_out out_channel in
-  { log; wrap_up }
+  object
+    val formatter = formatter
 
-let database_reporter () =
-  let () = if Sys.file_exists "db.log" then Sys.remove "db.log" in
-  let database = Sanddb.create_json_database "db.log" (module Report_j) in
-  let serialize_content : 'a. 'a Report.content -> string = function
-    | Debug msgf  ->
-        let str = ref "" in
-        (msgf @@ fun fmt -> Format.kasprintf (fun s -> str := s) fmt);
-        !str
-    | Phase phase -> Format.asprintf "Phase %s" @@ Report.string_of_phase phase
-  in
-  let serialize_severity : Report.severity -> Report_t.severity = function
-    | Info    -> `Info
-    | Log     -> `Log
-    | Success -> `Success
-    | Error   -> `Error
-    | Warning -> `Warning
-  in
-  let log : 'a. 'a Report.t -> unit =
-   fun report ->
-    let report : Report_t.t =
-      {
-        id = Uuidm.to_string report.id;
-        title = report.title;
-        elapsed_time = report.elapsed_time;
-        previous = Option.map Uuidm.to_string report.previous;
-        parent = Option.map Uuidm.to_string report.parent;
-        content = serialize_content report.content;
-        severity = serialize_severity report.severity;
-      }
-    in
-    let _ = Sanddb.insert_record database report in
-    ()
-  in
-  let wrap_up () = () in
-  { log; wrap_up }
+    val out_channel = out_channel
 
-let reporters = ref [ file_reporter (); database_reporter () ]
+    method log : 'a. 'a Report.t -> unit =
+      fun report ->
+        match report.content with
+        | Debug msgf  ->
+            msgf @@ fun fmt -> Format.fprintf formatter @@ fmt ^^ "@,@?"
+        | Phase phase ->
+            Format.fprintf formatter "*** Phase %s ***@,@?"
+            @@ Report.string_of_phase phase
 
-let log report = List.iter (fun reporter -> reporter.log report) !reporters
+    method wrap_up () = close_out out_channel
+  end
 
-let wrap_up () = List.iter (fun reporter -> reporter.wrap_up ()) !reporters
+class database_reporter () =
+  object (self)
+    val database =
+      if Sys.file_exists "db.log" then Sys.remove "db.log";
+      Sanddb.create_json_database "db.log" (module Report_j)
+
+    method private serialize_content : 'a. 'a Report.content -> string =
+      function
+      | Debug msgf  ->
+          let str = ref "" in
+          (msgf @@ fun fmt -> Format.kasprintf (fun s -> str := s) fmt);
+          !str
+      | Phase phase ->
+          Format.asprintf "Phase %s" @@ Report.string_of_phase phase
+
+    method private serialize_severity : Report.severity -> Report_t.severity =
+      function
+      | Info    -> `Info
+      | Log     -> `Log
+      | Success -> `Success
+      | Error   -> `Error
+      | Warning -> `Warning
+
+    method log : 'a. 'a Report.t -> unit =
+      fun report ->
+        let report : Report_t.t =
+          {
+            id = Uuidm.to_string report.id;
+            title = report.title;
+            elapsed_time = report.elapsed_time;
+            previous = Option.map Uuidm.to_string report.previous;
+            parent = Option.map Uuidm.to_string report.parent;
+            content = self#serialize_content report.content;
+            severity = self#serialize_severity report.severity;
+          }
+        in
+        let _ = Sanddb.insert_record database report in
+        ()
+
+    method wrap_up () = ()
+  end
+
+let reporters : t list ref =
+  ref [ new file_reporter (); new database_reporter () ]
+
+let log report =
+  List.iter (fun (reporter : t) -> reporter#log report) !reporters
+
+let wrap_up () = List.iter (fun reporter -> reporter#wrap_up ()) !reporters
