d3d61092734ca35df85cc34b4c79ad1a6e44ea06
Radu Lacraru <radu.lacraru97@gmail.com>
2020-05-14 13:01:43 +0300

    Logging: introduce target language specific logs
    Signed-off-by: Radu Lacraru <radu.lacraru97@gmail.com>

--- GillianCore/logging/databaseReporter.ml
+++ GillianCore/logging/databaseReporter.ml
@@ -22,10 +22,21 @@ end)
 
 let get_database () = (get_state ()).database
 
-class t =
+class virtual ['a] t =
   object (self)
-    method log (report : Report.t) =
+    method log_agnostic : 'a. (Report.agnostic, 'a) Report.t -> unit =
+      fun report ->
+        if enabled () then
+          self#insert_report report
+          @@ self#serialize_agnostic_content report.content
+
+    method log_specific (report : (Report.specific, 'a) Report.t) =
       if enabled () then
+        self#insert_report report
+        @@ self#serialize_specific_content report.content
+
+    method private insert_report : 'a 'b. ('a, 'b) Report.t -> string -> unit =
+      fun report content ->
         let report : Report_t.t =
           {
             id = Uuidm.to_string report.id;
@@ -33,17 +44,11 @@ class t =
             elapsed_time = report.elapsed_time;
             previous = Option.map Uuidm.to_string report.previous;
             parent = Option.map Uuidm.to_string report.parent;
-            content = self#serialize_content report.content;
+            content;
             severity = self#serialize_severity report.severity;
           }
         in
         ignore @@ Sanddb.insert_record self#database report
-
-    method private serialize_content : Report.content -> string =
-      function
-      | Debug msgf  -> Report.PackedPP.str msgf
-      | Phase phase ->
-          Format.asprintf "Phase %s" @@ Report.string_of_phase phase
 
     method private serialize_severity : Report.severity -> Report_t.severity =
       function
@@ -54,6 +59,20 @@ class t =
       | Warning -> `Warning
 
     method private database = get_database ()
+
+    method private serialize_agnostic_content
+        : 'a. (Report.agnostic, 'a) Report.content -> string =
+      function
+      | Debug msgf  -> Report.PackedPP.str msgf
+      | Phase phase ->
+          Format.asprintf "Phase %s" @@ Report.string_of_phase phase
+
+    method private serialize_specific_content
+        : (Report.specific, 'a) Report.content -> string =
+      function
+      | TargetLang tl -> self#serialize_target_lang tl
+
+    method virtual private serialize_target_lang : 'a -> string
 
     method wrap_up = wrap_up ()
   end
--- GillianCore/logging/default.ml
+++ GillianCore/logging/default.ml
@@ -1,6 +1,25 @@
-let reporters =
-  [ (new FileReporter.t :> Reporter.t); (new DatabaseReporter.t :> Reporter.t) ]
+type t
 
-let log report = List.iter (fun reporter -> reporter#log report) reporters
+let file_reporter =
+  object
+    inherit [t] FileReporter.t
+
+    method private target_lang _ = ()
+  end
+
+let database_reporter =
+  object
+    inherit [t] DatabaseReporter.t
+
+    method private serialize_target_lang _ = ""
+  end
+
+let reporters =
+  [ (file_reporter :> t Reporter.t); (database_reporter :> t Reporter.t) ]
+
+let log report =
+  List.iter
+    (fun (reporter : t Reporter.t) -> reporter#log_agnostic report)
+    reporters
 
 let wrap_up () = List.iter (fun reporter -> reporter#wrap_up) reporters
