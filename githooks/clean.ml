0ae432d9d2bc91e7786a2474954b51eed6f29f9c
Sacha Ayoun <sachaayoun@gmail.com>
2020-05-20 17:22:42 +0100

    ocaml script instead of python script for installing git hooks (#52)
    * better hook handling, needs fix
    
    * fix install-githooks
    
    * Printing
    
    * Add newline
    
    Co-authored-by: Alexis Marinoiu <alexis.marinoiu16@imperial.ac.uk>

---
+++ githooks/clean.ml
@@ -0,0 +1,16 @@
+#!/usr/bin/env ocaml
+
+#use "topfind";;
+#thread
+
+#require "shexp.process";;
+#use "./githooks/common.ml";;
+#require "fileutils"
+
+let abshooks = Stdlib.List.map (Filename.concat destination) hooks_names
+
+let () =
+  Stdlib.List.iter (Printf.printf "Cleaning %s\n") abshooks
+
+let () = FileUtil.rm abshooks
+
--- githooks/install.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-import os
-from common import *
-
-for f in hook_names:
-  absdest = os.path.join(destination, f)
-  copyfile(os.path.join(githooksFolder, f), absdest)
-  os.chmod(absdest, os.stat(absdest).st_mode | stat.S_IEXEC)
