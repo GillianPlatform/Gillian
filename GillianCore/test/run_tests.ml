b59561710a2b4e0ab9dd4d1361dc08cb95918b8a
pmaksimo <p.maksimovic@imperial.ac.uk>
2020-09-30 10:52:20 +0100

    no more up_tests

--- GillianCore/test/run_tests.ml
+++ GillianCore/test/run_tests.ml
@@ -1,7 +1,4 @@
 let test_suites : unit Alcotest.test list =
-  [
-    ("Gil_syntax.Reducers", Gil_syntax_tests.Visitors.tests);
-    ("Engine.UP", Up_tests.tests);
-  ]
+  [ ("Gil_syntax.Reducers", Gil_syntax_tests.Visitors.tests) ]
 
 let () = Alcotest.run "Gillian" test_suites
