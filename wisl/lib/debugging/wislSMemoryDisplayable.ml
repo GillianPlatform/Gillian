1a71058c780619d47206eb4254987b4b018f9c6b
Matthew Ho <matthew_ho@hotmail.co.uk>
2021-05-06 10:40:08 +0100

    Only display offset in Leafs for SMemory display as location is already displayed as parent node

--- wisl/lib/debugging/wislSMemoryDisplayable.ml
+++ wisl/lib/debugging/wislSMemoryDisplayable.ml
@@ -7,15 +7,10 @@ type t = WislSMemory.t
 let to_debugger_tree (heap : t) =
   let bindings = WislSMemory.bindings heap in
   let vstr = WPrettyUtils.to_str Values.pp in
-  let loc_nodes loc l : debugger_tree list =
-    List.map
-      (fun (offset, value) ->
-        let loc_offset = Printf.sprintf "(%s, %s)" loc (vstr offset) in
-        let value = vstr value in
-        Leaf (loc_offset, value))
-      l
+  let loc_nodes l : debugger_tree list =
+    List.map (fun (offset, value) -> Leaf (vstr offset, vstr value)) l
   in
   let all_loc_nodes =
-    List.map (fun (loc, assocs) -> Node (loc, loc_nodes loc assocs)) bindings
+    List.map (fun (loc, assocs) -> Node (loc, loc_nodes assocs)) bindings
   in
   all_loc_nodes
