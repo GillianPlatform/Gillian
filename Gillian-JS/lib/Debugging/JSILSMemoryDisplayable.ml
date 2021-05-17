3c30bb36308767ec8f82344e135bf86ceb4aa25c
Matthew Ho <matthew_ho@hotmail.co.uk>
2021-05-17 12:22:18 +0100

    Display JS properties as list

--- Gillian-JS/lib/Debugging/JSILSMemoryDisplayable.ml
+++ Gillian-JS/lib/Debugging/JSILSMemoryDisplayable.ml
@@ -6,11 +6,16 @@ type t = Semantics.Symbolic.t
 
 let to_debugger_tree (smemory : t) : debugger_tree list =
   let sorted_locs_with_vals = Symbolic.sorted_locs_with_vals smemory in
-  let to_str pp value = Fmt.to_to_string (Fmt.hbox pp) value in
+  let to_str pp = Fmt.to_to_string (Fmt.hbox pp) in
   let property_nodes fv_pairs : debugger_tree list =
     fv_pairs |> Expr.YojsonableMap.to_seq
     |> Seq.map (fun (name, value) ->
-           Leaf (to_str Expr.pp name, to_str Expr.pp value))
+           let name = to_str Expr.pp name in
+           match value with
+           | Expr.EList lst -> list_to_debugger_tree name (to_str Expr.pp) lst
+           | Expr.Lit (Literal.LList lst) ->
+               list_to_debugger_tree name (to_str Literal.pp) lst
+           | _ -> Leaf (name, to_str Expr.pp value))
     |> List.of_seq
   in
   let value_nodes ((fv_pairs, domain), metadata) : debugger_tree list =
--- GillianCore/debugger/displayable.ml
+++ GillianCore/debugger/displayable.ml
@@ -10,3 +10,12 @@ module type S = sig
   (** Converts type into a debugger tree which can be displayed in the debugger *)
   val to_debugger_tree : t -> debugger_tree list
 end
+
+let list_to_debugger_tree
+    (name : string) (to_string : 'a -> string) (lst : 'a list) : debugger_tree =
+  let leaves =
+    List.mapi
+      (fun index element -> Leaf (string_of_int index, to_string element))
+      lst
+  in
+  Node (name, leaves)
