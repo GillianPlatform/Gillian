fbab4030dff20c09b7286f288c0e3adf746c7a04
Matthew Ho <matthew_ho@hotmail.co.uk>
2021-05-19 16:57:58 +0100

    Fix display of wisl pointers

--- wisl/lib/debugging/wislDisplayFilter.ml
+++ wisl/lib/debugging/wislDisplayFilter.ml
@@ -1,16 +1,25 @@
+open Gil_syntax
+
 let filter_map_store store =
   List.filter_map
     (fun (var, (value : Gil_syntax.Expr.t)) ->
       if Str.string_match (Str.regexp "gvar") var 0 then None
       else
+        let match_offset lst loc loc_pp =
+          match lst with
+          | [ Expr.Lit (Num offset) ] ->
+              Fmt.str "-> (%a, %.0f)" (Fmt.hbox loc_pp) loc offset
+          | [ offset ]                ->
+              Fmt.str "-> (%a, %a)" (Fmt.hbox loc_pp) loc (Fmt.hbox Expr.pp)
+                offset
+          | _                         -> Fmt.to_to_string (Fmt.hbox Expr.pp)
+                                           value
+        in
         let value =
           match value with
-          | Gil_syntax.Expr.EList [ Lit (Loc loc); Lit (Num offset) ] ->
-              (* Displaying of a pointer in WISL *)
-              Fmt.str "-> (%a, %.0f)"
-                (Fmt.hbox Gil_syntax.Expr.pp)
-                (Gil_syntax.Expr.Lit (Loc loc)) offset
-          | value -> Fmt.to_to_string (Fmt.hbox Gil_syntax.Expr.pp) value
+          | Expr.EList (Lit (Loc loc) :: rest) | Expr.EList (LVar loc :: rest)
+            -> match_offset rest loc Fmt.string
+          | _ -> Fmt.to_to_string (Fmt.hbox Expr.pp) value
         in
         Some (var, value))
     store
