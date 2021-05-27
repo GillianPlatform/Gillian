a107481fa208a6d4877843ac7ce11822c46add8b
Matthew Ho <matthew_ho@hotmail.co.uk>
2021-05-27 11:27:44 +0100

    Ignore debug messages when reporting to database to improve performance

--- GillianCore/logging/databaseReporter.ml
+++ GillianCore/logging/databaseReporter.ml
@@ -7,24 +7,26 @@ let initialize () =
   LogDatabase.create_db ()
 
 let log (report : Report.t) =
-  let id = Sqlite3.Data.TEXT (Uuidm.to_string report.id) in
-  let title = Sqlite3.Data.TEXT report.title in
-  let elapsed_time = Sqlite3.Data.FLOAT report.elapsed_time in
-  let previous =
-    Sqlite3.Data.opt_text (Option.map Uuidm.to_string report.previous)
-  in
-  let parent =
-    Sqlite3.Data.opt_text (Option.map Uuidm.to_string report.parent)
-  in
-  let content =
-    Sqlite3.Data.TEXT
-      (Yojson.Safe.to_string (Loggable.loggable_to_yojson report.content))
-  in
-  let severity =
-    Sqlite3.Data.INT (Int64.of_int (Report.severity_to_enum report.severity))
-  in
-  let type_ = Sqlite3.Data.TEXT report.type_ in
-  LogDatabase.store_report ~id ~title ~elapsed_time ~previous ~parent ~content
-    ~severity ~type_
+  if report.type_ <> LoggingConstants.ContentType.debug then
+    let id = Sqlite3.Data.TEXT (Uuidm.to_string report.id) in
+    let title = Sqlite3.Data.TEXT report.title in
+    let elapsed_time = Sqlite3.Data.FLOAT report.elapsed_time in
+    let previous =
+      Sqlite3.Data.opt_text (Option.map Uuidm.to_string report.previous)
+    in
+    let parent =
+      Sqlite3.Data.opt_text (Option.map Uuidm.to_string report.parent)
+    in
+    let content =
+      Sqlite3.Data.TEXT
+        (Yojson.Safe.to_string (Loggable.loggable_to_yojson report.content))
+    in
+    let severity =
+      Sqlite3.Data.INT (Int64.of_int (Report.severity_to_enum report.severity))
+    in
+    let type_ = Sqlite3.Data.TEXT report.type_ in
+
+    LogDatabase.store_report ~id ~title ~elapsed_time ~previous ~parent ~content
+      ~severity ~type_
 
 let wrap_up () = LogDatabase.close_db ()
