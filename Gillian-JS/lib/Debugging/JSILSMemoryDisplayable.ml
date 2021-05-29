a3fa157f13625bca25149287c69ca919c8421737
Matthew Ho <matthew_ho@hotmail.co.uk>
2021-05-29 20:02:06 +0100

    Add attempt to fold JS heap variables

--- Gillian-JS/lib/Debugging/JSILSMemoryDisplayable.ml
+++ Gillian-JS/lib/Debugging/JSILSMemoryDisplayable.ml
@@ -4,11 +4,105 @@ open Semantics
 
 type t = Semantics.Symbolic.t
 
-let to_debugger_tree (smemory : t) : debugger_tree list =
-  let sorted_locs_with_vals = Symbolic.sorted_locs_with_vals smemory in
-  let to_str pp = Fmt.to_to_string (Fmt.hbox pp) in
-  let property_nodes fv_pairs : debugger_tree list =
-    fv_pairs |> Expr.YojsonableMap.to_seq
+let to_str pp = Fmt.to_to_string (Fmt.hbox pp)
+
+let rec location_to_debugger_tree (name : string) (loc : string) (smemory : t) :
+    debugger_tree =
+  let rec lit_to_debugger_tree name lit =
+    match lit with
+    | Literal.Loc loc -> location_to_debugger_tree name loc smemory
+    | LList lst       ->
+        let nodes =
+          List.mapi
+            (fun index element ->
+              let name = string_of_int index in
+              lit_to_debugger_tree name element)
+            lst
+        in
+        Node (name, nodes)
+    | _               -> Leaf (name, to_str Literal.pp lit)
+  in
+  let rec expr_to_debugger_tree name expr =
+    match expr with
+    | Expr.ALoc loc  -> location_to_debugger_tree name loc smemory
+    | Expr.EList lst ->
+        let nodes =
+          List.mapi
+            (fun index element ->
+              expr_to_debugger_tree (string_of_int index) element)
+            lst
+        in
+        Node (name, nodes)
+    | Expr.Lit lit   -> lit_to_debugger_tree name lit
+    | _              -> Leaf (name, to_str Expr.full_pp expr)
+  in
+  let properties_to_debugger_tree properties =
+    let property_nodes =
+      properties |> Expr.YojsonableMap.to_seq
+      |> Seq.map (fun (name, value) ->
+             let name = to_str Expr.pp name in
+             let field_names =
+               [
+                 "descriptor_type";
+                 "value";
+                 "enumerable";
+                 "writable";
+                 "configurable";
+               ]
+             in
+             match value with
+             | Expr.EList lst ->
+                 let nodes =
+                   List.map2
+                     (fun field_name element ->
+                       expr_to_debugger_tree field_name element)
+                     field_names lst
+                 in
+                 Node (name, nodes)
+             | Expr.Lit (Literal.LList lst) ->
+                 let nodes = List.map2 lit_to_debugger_tree field_names lst in
+                 Node (name, nodes)
+             | _ -> expr_to_debugger_tree name value)
+      |> List.of_seq
+    in
+    Node ("properties", property_nodes)
+  in
+  match SHeap.get smemory loc with
+  | None -> Leaf (name, loc)
+  | Some ((properties, domain), metadata_opt) ->
+      (* Remove and add back location to prevent cycles in the debugger tree.
+         The better way to do this would be to have Debugger.Displayable convert
+         into a Hashtbl (i.e. similar to scopes table in debugger.ml which handles
+         where nodes in the debugger tree have new ids in the scopes table and
+         can be lazily loaded by VSCode). *)
+      let () = SHeap.remove smemory loc in
+      let metadata_node metadata_opt =
+        let name = "metadata" in
+        match metadata_opt with
+        | None          -> Leaf (name, "unknown")
+        | Some metadata -> (
+            match metadata with
+            | Expr.ALoc child_loc ->
+                location_to_debugger_tree name child_loc smemory
+            | _                   -> Leaf
+                                       (name, Fmt.to_to_string Expr.pp metadata)
+            )
+      in
+      let node =
+        Node
+          ( name ^ ": " ^ loc,
+            [
+              properties_to_debugger_tree properties;
+              Leaf ("domain", to_str (Fmt.option Expr.pp) domain);
+              metadata_node metadata_opt;
+            ] )
+      in
+      let () = SHeap.set smemory loc properties domain metadata_opt in
+      node
+
+let properties_to_debugger_tree properties =
+  let property_nodes =
+    properties |> Expr.YojsonableMap.to_seq
     |> Seq.map (fun (name, value) ->
            let name = to_str Expr.pp name in
            match value with
@@ -18,9 +112,14 @@ let to_debugger_tree (smemory : t) : debugger_tree list =
            | _ -> Leaf (name, to_str Expr.pp value))
     |> List.of_seq
   in
-  let value_nodes ((fv_pairs, domain), metadata) : debugger_tree list =
+  Node ("properties", property_nodes)
+
+let to_debugger_tree (smemory : t) : debugger_tree list =
+  let sorted_locs_with_vals = Symbolic.sorted_locs_with_vals smemory in
+  let to_str pp = Fmt.to_to_string (Fmt.hbox pp) in
+  let value_nodes ((properties, domain), metadata) : debugger_tree list =
     [
-      Node ("properties", property_nodes fv_pairs);
+      properties_to_debugger_tree properties;
       Leaf ("domain", to_str (Fmt.option Expr.pp) domain);
       Leaf
         ( "metadata",
@@ -30,3 +129,4 @@ let to_debugger_tree (smemory : t) : debugger_tree list =
   List.map
     (fun (loc, value) -> Node (loc, value_nodes value))
     sorted_locs_with_vals
+  @ [ location_to_debugger_tree "Global scope" "$lg" smemory ]
