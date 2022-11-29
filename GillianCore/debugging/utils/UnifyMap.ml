30f99092fa9a8a2254ce0f6c3c29114eba27db39
Nat Karmios <nat@karmios.com>
2022-11-29 12:14:04 +0000

    Document UnifyMap

--- GillianCore/debugging/utils/UnifyMap.ml
+++ GillianCore/debugging/utils/UnifyMap.ml
@@ -2,43 +2,51 @@ module L = Logging
 module DL = Debugger_log
 open Syntaxes.Option
 
-type rid = L.ReportId.t [@@deriving show, yojson]
 type unify_result = Success | Failure [@@deriving yojson]
 
 module Make (Verification : Verifier.S) = struct
   open Verification.SUnifier.Logging
   open L.LoggingConstants
 
+  (** Describes {i why} this unification is happening *)
   type kind = Unifier.unify_kind [@@deriving yojson]
 
+  (** A substitution, and the ID of the assertion where it was learned *)
   type substitution = {
-    assert_id : rid; [@key "assertId"]
+    assert_id : L.ReportId.t; [@key "assertId"]
     subst : string * string;
   }
   [@@deriving yojson]
 
+  (** Represents one step of a unification *)
   type assertion_data = {
-    id : rid;
-    fold : (rid * unify_result) option;
+    id : L.ReportId.t;
+    fold : (L.ReportId.t * unify_result) option;
     assertion : string;
     substitutions : substitution list;
   }
   [@@deriving yojson]
 
+  (** A segment of unification *)
   type unify_seg =
-    | Assertion of assertion_data * unify_seg
-    | UnifyResult of rid * unify_result
+    | Assertion of assertion_data * unify_seg  (** A single assertion *)
+    | UnifyResult of L.ReportId.t * unify_result
+        (** The end of this unification segment *)
   [@@deriving yojson]
 
+  (** A unification map.
+    Unification is either a single segment in the normal case, or potentially multiple segments when folding (i.e. when a predicate has multiple cases) *)
   type map = Direct of unify_seg | Fold of unify_seg list [@@deriving yojson]
+
   type t = kind * map [@@deriving yojson]
 
+  (** Given the ID of a unification, queries the log database to determine wither that unification was successful *)
   let result_of_id id =
     let rec aux id =
       let children = L.LogQueryer.get_children_of id in
       if children = [] then
-        Fmt.failwith "UnifyMap.result_of_id: report %a has no children!" pp_rid
-          id;
+        Fmt.failwith "UnifyMap.result_of_id: report %a has no children!"
+          L.ReportId.pp id;
       match
         children
         |> List.find_opt (fun (_, type_, _) -> type_ = ContentType.unify_result)
@@ -51,14 +59,20 @@ module Make (Verification : Verifier.S) = struct
           match result_report with
           | Success _ -> true
           | Failure _ -> false)
-      | None -> children |> List.exists (fun (id, _, _) -> aux id)
+      | None ->
+          children
+          |> List.exists (fun (id, type_, _) ->
+                 if type_ = ContentType.unify_case then aux id else false)
     in
     if aux id then Success else Failure
 
+  (** Traverses a [unify_seg] to get its result *)
   let rec seg_result = function
     | Assertion (_, next) -> seg_result next
     | UnifyResult (_, result) -> result
 
+  (** Gets the result of a unification.
+    When the unification is a fold, one successful case is sufficient for overall success *)
   let result = function
     | _, Direct seg -> seg_result seg
     | _, Fold segs ->
@@ -66,6 +80,7 @@ module Make (Verification : Verifier.S) = struct
           Success
         else Failure
 
+  (** Given the ID of an assertion or unify result, build the representative [unify_seg] *)
   let rec build_seg ?(prev_substs = []) (id, type_, content) : unify_seg =
     let module Subst = Verification.SUnifier.ESubst in
     if type_ = ContentType.assertion then
@@ -92,21 +107,21 @@ module Make (Verification : Verifier.S) = struct
           | [ child ] -> Some child
           | _ ->
               Fmt.failwith
-                "UnifyMap.build_seg: assertion %a has multiple children!" pp_rid
-                id
+                "UnifyMap.build_seg: assertion %a has multiple children!"
+                L.ReportId.pp id
         in
         if type_ <> ContentType.unify then
           Fmt.failwith
             "UnifyMap.build_seg: report %a (child of assertion %a) has type %s \
              (expected %s)!"
-            pp_rid child_id pp_rid id type_ ContentType.unify;
+            L.ReportId.pp child_id L.ReportId.pp id type_ ContentType.unify;
         (child_id, result_of_id child_id)
       in
       let seg =
         match L.LogQueryer.get_next_report_ids id with
         | [] ->
-            Fmt.failwith "UnifyMap.build_seg: assertion %a has no next!" pp_rid
-              id
+            Fmt.failwith "UnifyMap.build_seg: assertion %a has no next!"
+              L.ReportId.pp id
         | [ next_id ] ->
             let content, type_ =
               L.LogQueryer.get_report next_id |> Option.get
@@ -114,7 +129,7 @@ module Make (Verification : Verifier.S) = struct
             build_seg ~prev_substs:substitutions (next_id, type_, content)
         | _ ->
             Fmt.failwith "UnifyMap.build_seg: assertion %a has multiple nexts!"
-              pp_rid id
+              L.ReportId.pp id
       in
       Assertion ({ id; fold; assertion; substitutions }, seg)
     else if type_ = ContentType.unify_result then
@@ -131,16 +146,20 @@ module Make (Verification : Verifier.S) = struct
     else
       Fmt.failwith
         "UnifyMap.build_seg: report %a has invalid type (%s) for unify_seg!"
-        pp_rid id type_
+        L.ReportId.pp id type_
 
+  (** Given the ID of a (non-fold) unification or a unify case (when folding), build the representative [unify_seg] *)
   let build_case id : unify_seg =
     match L.LogQueryer.get_children_of ~roots_only:true id with
-    | [] -> Fmt.failwith "UnifyMap.build_case: id %a has no children!" pp_rid id
+    | [] ->
+        Fmt.failwith "UnifyMap.build_case: id %a has no children!" L.ReportId.pp
+          id
     | [ child ] -> build_seg child
     | _ ->
-        Fmt.failwith "UnifyMap.build_case: id %a has multiple children!" pp_rid
-          id
+        Fmt.failwith "UnifyMap.build_case: id %a has multiple children!"
+          L.ReportId.pp id
 
+  (** Given the ID of a folding unification, build the representative [unify_seg] of each case *)
   let build_cases id : unify_seg list =
     let rec aux id acc =
       let acc = build_case id :: acc in
@@ -149,15 +168,16 @@ module Make (Verification : Verifier.S) = struct
       | [ (id, type_, _) ] ->
           if type_ <> ContentType.unify_case then
             Fmt.failwith "UnifyMap.build_cases: %a has type %s (expected %s)!"
-              pp_rid id type_ ContentType.unify_case
+              L.ReportId.pp id type_ ContentType.unify_case
           else aux id acc
       | _ ->
           Fmt.failwith "UnifyMap.build_cases: unify_case %a has multiple nexts!"
-            pp_rid id
+            L.ReportId.pp id
     in
 
     aux id [] |> List.rev
 
+  (** Given the ID of a unification, build the representative unification map *)
   let build unify_id =
     let kind =
       let content, _ = L.LogQueryer.get_report unify_id |> Option.get in
@@ -173,8 +193,8 @@ module Make (Verification : Verifier.S) = struct
         | [ child ] -> child
         | _ ->
             Fmt.failwith
-              "UnifyMap.build: unify id %a should have one root child!" pp_rid
-              unify_id
+              "UnifyMap.build: unify id %a should have one root child!"
+              L.ReportId.pp unify_id
       in
       if type_ = ContentType.unify_case then
         let segs = build_cases id in
