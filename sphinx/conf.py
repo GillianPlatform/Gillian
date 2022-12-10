10101429d6944992464eb6dc70392198aefca4db
Nat Karmios <nat@karmios.com>
2022-12-10 01:12:04 +0000

    Add style to code blocks in Sphinx docs

--- sphinx/conf.py
+++ sphinx/conf.py
@@ -116,6 +116,7 @@ html_theme_options = {
   'light_css_variables': {
     'color-brand-primary': '#b5393a',
     'color-brand-content': '#437d89',
+    'color-code-background': '#f6f8fa',
   },
   'dark_css_variables': {
     'color-brand-primary': '#e9494b',
