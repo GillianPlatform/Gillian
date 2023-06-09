c0de34a7926552f52b9004993f3ad11cca49514d
Sacha Ayoun <sachaayoun@gmail.com>
2023-06-09 09:23:00 +0100

    add entails and check_sat to the Delayed api (#253)
    Signed-off-by: Sacha Ayoun <sachaayoun@gmail.com>

--- GillianCore/monadic/delayed.mli
+++ GillianCore/monadic/delayed.mli
@@ -9,6 +9,8 @@ val return :
 
 val resolve_loc : Expr.t -> string option t
 val reduce : Expr.t -> Expr.t t
+val entails : Formula.t list -> Formula.t -> bool t
+val check_sat : Formula.t -> bool t
 val bind : 'a t -> ('a -> 'b t) -> 'b t
 val map : 'a t -> ('a -> 'b) -> 'b t
 val vanish : unit -> 'a t
