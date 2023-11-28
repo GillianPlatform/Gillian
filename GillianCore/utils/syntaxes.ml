22681c1ccc38ef681f21da3d6b6eadf2e39c07c1
Nat Karmios <nat@karmios.com>
2023-11-28 11:28:11 +0000

    Update various utils

--- GillianCore/utils/syntaxes.ml
+++ GillianCore/utils/syntaxes.ml
@@ -16,8 +16,43 @@ end
 
 (** @canonical Gillian.Utils.Syntaxes.Option *)
 module Option = struct
-  let ( let+ ) f o = Option.map o f
+  let ( let+ ) o f = Option.map f o
   let ( let* ) o f = Option.bind o f
+
+  let ( let- ) o f =
+    match o with
+    | None -> f ()
+    | Some o -> o
+
+  let ( let/ ) o f =
+    match o with
+    | None -> f ()
+    | o -> o
+end
+
+(** @canonical Gillian.Utils.Syntaxes.Result_of_option *)
+module Result_of_option = struct
+  let ( let=* ) o f =
+    match o with
+    | Ok (Some o) -> f o
+    | Ok None -> Ok None
+    | Error e -> Error e
+
+  let ( let=+ ) o f =
+    match o with
+    | Ok (Some o) -> Ok (Some (f o))
+    | Ok None -> Ok None
+    | Error e -> Error e
+
+  let ( let=/ ) o f =
+    match o with
+    | Ok None -> f ()
+    | o -> o
+
+  let ( let=- ) o f =
+    match o with
+    | Ok None -> Ok (Some (f ()))
+    | o -> o
 end
 
 (** @canonical Gillian.Utils.Syntaxes.List *)
