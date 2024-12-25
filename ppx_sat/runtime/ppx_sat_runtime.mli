f4746f0b43e7f468b8ed29c6419912a641d5a359
N1ark <n1ark.officiel@gmail.com>
2024-12-25 12:52:24 +0100

    Update ppx_sat

--- ppx_sat/runtime/ppx_sat_runtime.mli
+++ ppx_sat/runtime/ppx_sat_runtime.mli
@@ -2,10 +2,10 @@ open Monadic.Delayed
 open Gil_syntax
 
 val if_then_else :
-  Formula.t -> then_branch:(unit -> 'a t) -> else_branch:(unit -> 'a t) -> 'a t
+  Expr.t -> then_branch:(unit -> 'a t) -> else_branch:(unit -> 'a t) -> 'a t
 
 val if_sure_then_else :
-  Formula.t -> then_branch:(unit -> 'a t) -> else_branch:(unit -> 'a t) -> 'a t
+  Expr.t -> then_branch:(unit -> 'a t) -> else_branch:(unit -> 'a t) -> 'a t
 
-val branch_entailment : (Formula.t * (unit -> 'a t)) list -> 'a t
-val true_formula : Gil_syntax.Formula.t
+val branch_entailment : (Expr.t * (unit -> 'a t)) list -> 'a t
+val true_formula : Expr.t
