b05f12c32f9f4bb703606d0c72949afd86f1e5bb
2over12 <ian.smith@trailofbits.com>
2025-01-10 16:56:52 -0500

    remove chunks

--- Gillian-LLVM/lib/memory_model/predicates.ml
+++ Gillian-LLVM/lib/memory_model/predicates.ml
@@ -1,4 +1,5 @@
 open Gil_syntax
+open Constants
 
 module Core = struct
   let pred ga ins outs =
@@ -6,12 +7,12 @@ module Core = struct
     Asrt.GA (ga_name, ins, outs)
 
   let single ~loc ~ofs ~chunk ~sval ~perm =
-    let chunk = Expr.Lit (String (Chunk.to_string chunk)) in
+    let chunk = Expr.Lit (Int (Z.of_int chunk)) in
     let perm = Expr.Lit (String (Perm.opt_to_string perm)) in
     pred (GMem Single) [ loc; ofs; chunk ] [ sval; perm ]
 
   let array ~loc ~ofs ~chunk ~size ~sval_arr ~perm =
-    let chunk = Expr.Lit (String (Chunk.to_string chunk)) in
+    let chunk = Expr.Lit (Int (Z.of_int chunk)) in
     let perm = Expr.Lit (String (Perm.opt_to_string perm)) in
     pred (GMem Array) [ loc; ofs; size; chunk ] [ sval_arr; perm ]
 
@@ -33,8 +34,6 @@ module Core = struct
 end
 
 module Others = struct
-  open Kcommons.Constants
-
   let pred name params = Asrt.Pred (name, params)
 
   let malloced_abst ~ptr ~total_size =
@@ -52,7 +51,7 @@ module Others = struct
     pred Internal_Predicates.undefs_ptr_size [ ptr; size ]
 
   let array_ptr ~ptr ~chunk ~size ~content =
-    let chunk_str = Expr.string (Chunk.to_string chunk) in
+    let chunk_str = Expr.Lit (Int (Z.of_int chunk)) in
     pred Internal_Predicates.array_ptr [ ptr; size; chunk_str; content ]
 
   let ptr_add ~ptr ~to_add ~res =
