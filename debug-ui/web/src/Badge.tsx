f0a9ce73245b098ae215cd37f76cff406d3c4049
Nat Karmios <nat@karmios.com>
2025-01-21 17:58:23 -0700

    Remove debug-ui
    This has been moved to the sedap-js repo

--- debug-ui/web/src/Badge.tsx
+++
@@ -1,9 +0,0 @@
-import { ComponentOverrideProps } from "@sedap/react";
-import { VSCodeBadge } from "@vscode/webview-ui-toolkit/react";
-
-const Badge: React.FC<ComponentOverrideProps> = ({ className, style, children }) => {
-  const props = { className, style, children } as const;
-  return <VSCodeBadge {...props} />;
-};
-
-export default Badge;
