a9c1585a4f067fc67544f42e14acc70841d54047
Shiva Tamil Kumaran <shivatk01@gmail.com>
2025-04-09 15:05:50 +0100

    Fixed code style in WDatatype and WConstructor

--- wisl/lib/syntax/WConstructor.mli
+++ wisl/lib/syntax/WConstructor.mli
@@ -1,6 +1,6 @@
 type t = {
   constructor_name : string;
   constructor_fields : WType.t list;
-  constructor_loc: CodeLoc.t;
-  constructor_id: int;
+  constructor_loc : CodeLoc.t;
+  constructor_id : int;
 }
--- wisl/lib/syntax/WDatatype.mli
+++ wisl/lib/syntax/WDatatype.mli
@@ -2,5 +2,5 @@ type t = {
   datatype_name : string;
   datatype_constructors : WConstructor.t list;
   datatype_loc : CodeLoc.t;
-  datatype_id: int;
+  datatype_id : int;
 }
