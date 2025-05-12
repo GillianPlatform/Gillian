81f8aa535f8e316042d590e775db1c444fb3aa4f
Nat Karmios <nat@karmios.com>
2025-05-12 19:19:08 +0100

    Defer getting variables to lifters

--- Gillian-JS/lib/Debugging/JSLifter.ml
+++ Gillian-JS/lib/Debugging/JSLifter.ml
@@ -311,4 +311,33 @@ struct
         ]
       in
       scopes
+
+  let get_variables _ ~store ~memory ~pfs ~types ~preds _ =
+    let open Gil_lifter in
+    let open Variable in
+    let variables = Hashtbl.create 0 in
+    (* New scope ids must be higher than last top level scope id to prevent
+        duplicate scope ids *)
+    let scope_id = ref (List.length top_level_scopes) in
+    let get_new_scope_id () =
+      let () = scope_id := !scope_id + 1 in
+      !scope_id
+    in
+    let lifted_scopes =
+      let lifted_scopes =
+        add_variables ~store ~memory ~is_gil_file:false ~get_new_scope_id
+          variables
+      in
+      let pure_formulae_vars = get_pure_formulae_vars pfs in
+      let type_env_vars = get_type_env_vars types in
+      let pred_vars = get_pred_vars preds in
+      let vars_list = [ pure_formulae_vars; type_env_vars; pred_vars ] in
+      let () =
+        List.iter2
+          (fun (scope : scope) vars -> Hashtbl.replace variables scope.id vars)
+          top_level_scopes vars_list
+      in
+      lifted_scopes
+    in
+    (lifted_scopes, variables)
 end
--- wisl/lib/debugging/wislLifter.ml
+++ wisl/lib/debugging/wislLifter.ml
@@ -29,7 +29,8 @@ let ( let++ ) f o = Result.map o f
 let ( let** ) o f = Result.bind o f
 
 module Make
-    (Gil : Gillian.Debugger.Lifter.Gil_fallback_lifter.Gil_lifter_with_state)
+    (Gil : Gillian.Debugger.Lifter.Gil_fallback_lifter.Gil_lifter_with_state
+             with type Lifter.memory = WislSMemory.t)
     (Verification : Engine.Verifier.S with type annot = Annot.t) =
 struct
   open Exec_map
@@ -964,6 +965,35 @@ struct
     { id; description }
 
   let add_variables = WislSMemory.add_debugger_variables
+
+  let get_variables _ ~store ~memory ~pfs ~types ~preds _ =
+    let open Gil_lifter in
+    let open Variable in
+    let variables = Hashtbl.create 0 in
+    (* New scope ids must be higher than last top level scope id to prevent
+        duplicate scope ids *)
+    let scope_id = ref (List.length top_level_scopes) in
+    let get_new_scope_id () =
+      let () = scope_id := !scope_id + 1 in
+      !scope_id
+    in
+    let lifted_scopes =
+      let lifted_scopes =
+        add_variables ~store ~memory ~is_gil_file:false ~get_new_scope_id
+          variables
+      in
+      let pure_formulae_vars = get_pure_formulae_vars pfs in
+      let type_env_vars = get_type_env_vars types in
+      let pred_vars = get_pred_vars preds in
+      let vars_list = [ pure_formulae_vars; type_env_vars; pred_vars ] in
+      let () =
+        List.iter2
+          (fun (scope : scope) vars -> Hashtbl.replace variables scope.id vars)
+          top_level_scopes vars_list
+      in
+      lifted_scopes
+    in
+    (lifted_scopes, variables)
 
   let select_case nexts =
     let result =
