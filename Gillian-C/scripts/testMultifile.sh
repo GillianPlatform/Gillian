bf7995987f4f48a8df7d861ae13eb1be2cb1f247
Nat Karmios <nat@karmios.com>
2026-06-22 13:25:36 +0100

    C: Fix test scripts to work in dev environment

--- Gillian-C/scripts/testMultifile.sh
+++ Gillian-C/scripts/testMultifile.sh
@@ -2,13 +2,19 @@
 
 set -e
 
+if [[ "${GITHUB_ACTIONS}" ]]; then
+	GILLIAN_C="gillian-c"
+else
+  GILLIAN_C="dune exec -- gillian-c"
+fi
+
 # echo "--- executing main.c ---"
 # gillian-c exec -l disabled multifile/concrete/main.c multifile/concrete/foo.c
 # rc=$?; if [[ $rc != 0 ]]; then FINAL_RETURN=1; fi
 # printf "\n\n"
 
 echo "--- verifying BST ---"
-gillian-c verify -l disabled multifile/verification/sll_a.c multifile/verification/sll_b.c
+$GILLIAN_C verify -l disabled multifile/verification/sll_a.c multifile/verification/sll_b.c
 printf "\n\n"
 
 # echo "--- bi-abducing BST ---"
