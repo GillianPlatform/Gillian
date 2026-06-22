2644cb0af82a32b1776028321143cb4919b05150
Nat Karmios <nat@karmios.com>
2026-06-22 15:09:30 +0100

    C2: Clean up test scripts

--- Gillian-C2/scripts/testVerif.sh
+++ Gillian-C2/scripts/testVerif.sh
@@ -2,13 +2,20 @@
 
 FINAL_RETURN=0
 
+if [[ "${GITHUB_ACTIONS}" ]]; then
+	GILLIAN_C2="gillian-c2"
+else
+  GILLIAN_C2="dune exec -- gillian-c2"
+fi
+VERIFY="time $GILLIAN_C2 verify -l disabled"
+
 echo "--- verifying SLL ---"
-time gillian-c2 verify verification/sll.c
+$VERIFY verification/sll.c
 rc=$?; if [[ $rc != 0 ]]; then FINAL_RETURN=1; fi
 printf "\n\n"
 
 echo "--- verifying DLL ---"
-time gillian-c2 verify verification/dll.c
+$VERIFY verification/dll.c
 rc=$?; if [[ $rc != 0 ]]; then FINAL_RETURN=1; fi
 printf "\n\n"
 
