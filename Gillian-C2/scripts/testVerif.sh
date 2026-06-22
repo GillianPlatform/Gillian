ce89619976e919751f9fd813061891bf086bdd6e
Nat Karmios <nat@karmios.com>
2026-06-22 14:12:20 +0100

    C2: Test sll.c & dll.c instead of list_unbounded.gil & list_std.gil

--- Gillian-C2/scripts/testVerif.sh
+++ Gillian-C2/scripts/testVerif.sh
@@ -3,12 +3,12 @@
 FINAL_RETURN=0
 
 echo "--- verifying SLL ---"
-time gillian-c2 verify verification/list_unbounded.gil -a
+time gillian-c2 verify verification/sll.c
 rc=$?; if [[ $rc != 0 ]]; then FINAL_RETURN=1; fi
 printf "\n\n"
 
 echo "--- verifying DLL ---"
-time gillian-c2 verify verification/list_std.gil -a
+time gillian-c2 verify verification/dll.c
 rc=$?; if [[ $rc != 0 ]]; then FINAL_RETURN=1; fi
 printf "\n\n"
 
